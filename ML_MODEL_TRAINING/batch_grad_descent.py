035194613903d97ba5a1c46c17959145d73ae879
Kofi Dixon <dicksonsci@gmail.com>
2026-06-14 21:21:20 +0000

    Batch gradient descent

---
+++ ML_MODEL_TRAINING/batch_grad_descent.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+# training data with our inputs x and outputs y
+X = np.array([1, 2, 3])
+y = np.array([3, 5, 7])
+
+# this is our initial guesses for as parameters, theta 0 is the intercept or the bias term.
+theta0 = 1
+theta1 = 2
+
+learning_rate = 0.1
+epochs = 100
+
+m = len(X)
+
+for epoch in range(epochs):
+
+    # this is called, forward pass
+    y_pred = theta0 + theta1 * X
+
+    # Error
+    error = y_pred - y
+
+    # Gradients
+    d_theta0 = (1/m) * np.sum(error)
+
+    d_theta1 = (1/m) * np.sum(error * X)
+
+    # Update
+    theta0 -= learning_rate * d_theta0
+    theta1 -= learning_rate * d_theta1
+
+print(theta0, theta1)
