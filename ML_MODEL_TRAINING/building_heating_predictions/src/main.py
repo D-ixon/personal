5ddc6de8f8ff14cd3dd4fca5d6958bd279926986
Kofi Dixon <dicksonsci@gmail.com>
2026-06-18 21:39:54 +0000

    Task; Training completed

--- ML_MODEL_TRAINING/building_heating_predictions/src/main.py
+++ ML_MODEL_TRAINING/building_heating_predictions/src/main.py
@@ -4,27 +4,39 @@ from preprocess import preprocess_data
 from train_model import train_model
 from evaluate import evaluate
 from predict import predict
+import pandas as pds
 
 
 X_train, X_test, y_train, scaler = preprocess_data(train_df, test_df)
-
-
 X_train_biased = np.c_[np.ones(X_train.shape[0]), X_train]
 X_test_biased = np.c_[np.ones(X_test.shape[0]), X_test]
 
-
-alpha = 0.01
-iterations = 1000
+alpha = 0.001
+iterations = 5000
 
 theta, loss_history = train_model(X_train_biased, y_train, alpha, iterations)
 
-print("Training complete!")
-print("Final Weights (Theta):", theta)
+Y1_predict = predict(X_test_biased, theta)
 
+test_ids = test_df['ID']
+
+results_df = pds.DataFrame({
+    'ID': test_ids,
+    'Heating_Load': Y1_predict
+})
+
+results_df.to_csv('final_predictions.csv', index=False)
+print("Final predictions saved to final_predictions.csv")
+
+print("Training complete!")
+print ("                  ")
+print("Final Weights (Theta):", theta)
+print("                    ")
 rmse, _ = evaluate(X_train_biased, y_train, theta)
 print("Training RMSE:", rmse)
 
 test_predictions = predict(X_test_biased, theta)
-
+print("                    ")
 print("Test predictions (first 10):")
+print("                    ")
 print(test_predictions[:10])
