e2b523ced29d28500cfe7f6d79d81baaa32b5cc4
Kofi Dixon <dicksonsci@gmail.com>
2026-06-19 10:57:05 +0000

    Refactor: Simplify data parsing logic

--- ML_MODEL_TRAINING/building_heating_predictions/src/load_data.py
+++ ML_MODEL_TRAINING/building_heating_predictions/src/load_data.py
@@ -1,12 +1,4 @@
 import pandas as pds
-import os
 
-# Get the directory where load_data.py is located
-base_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Build the path dynamically
-train_path = os.path.join(base_dir, '..', 'data', 'Training_dataset.csv')
-test_path = os.path.join(base_dir, '..', 'data', 'test_dataset.csv')
-
-train_df = pds.read_csv(train_path)
-test_df = pds.read_csv(test_path)
+train_df = pds.read_csv('data/training_dataset.csv')
+test_df = pds.read_csv('data/test_dataset.csv')
