bb095b7ac7d6209008dc36f2747ab769b040ab16
Kofi Dixon <dicksonsci@gmail.com>
2026-06-26 16:59:52 +0000

    chore: turned dataframe to array and add columns of 1 to array

--- ML_MODEL_TRAINING/building_heating_predictions/src/normal_equation.py
+++ ML_MODEL_TRAINING/building_heating_predictions/src/normal_equation.py
@@ -1,5 +1,13 @@
 import pandas as pds
+import numpy as np
 
 train_ds = pds.read_csv('data/training_dataset.csv')
 test_ds = pds.read_csv('data/test_dataset.csv')
 
+X = train_ds.drop(columns=['ID', 'Y1'])
+y = train_ds['Y1']
+
+X_array = X.to_numpy()
+y_array = y.to_numpy()
+
+print(X_array.shape)
